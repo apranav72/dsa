daa2ad18e5a621bd1420071564fb1b8a5d3abfef
Pranav Agarkar <browsingpranav@gmail.com>
2024-07-03 20:50:02 +0530

    learned about loops

---
+++ java/lecture4/loops.java
@@ -0,0 +1,17 @@
+public class loops{
+	public static void main(String[] args){
+		//for(int i = 0; i<11; i++){
+		//	System.out.print(i+" ");
+		//}
+		
+		int i=0;
+		//while(i<11){
+		//	System.out.println(i);
+		//	i=i+1;	
+		//}
+		do{
+			System.out.println(i);
+			i=i+1;
+		}while(i<11);
+	}
+}
